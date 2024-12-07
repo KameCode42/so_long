b22b7fb317b52a8802c94bf96c9a7da33736679a
David <lefur.david@bluewin.ch>
2024-12-07 16:46:37 +0100

    add exit

--- src/move/exit.c
+++ src/move/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dle-fur <dle-fur@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 10:48:18 by david             #+#    #+#             */
-/*   Updated: 2024/12/05 13:14:46 by david            ###   ########.fr       */
+/*   Updated: 2024/12/07 16:44:33 by dle-fur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,8 @@ int	success_game(t_game *game)
 {
 	if (game->map[game->player_y][game->player_x] == EXIT)
 	{
-		if (game->count_items < game->total_items)
-			return (1);
-		else
-		{
-			ft_printf("Success: you finished the game\n");
-			close_game(game);
-		}
+		ft_printf("Success: you finished the game\n");
+		close_game(game);
 	}
 	return (0);
 }
